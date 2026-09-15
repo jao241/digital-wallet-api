5362dfbe58926d37910ff054097a4ba38a83102a
João Vitor Lima Lago Santos <vorque45@gmail.com>
2026-09-14 22:00:48 -0300

    feat: Adiçãode modulos para configuração e autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { PrismaModule } from './prisma/prisma.module.js';
 import { UserModule } from './user/user.module.js';
 import { WalletModule } from './wallet/wallet.module.js';
 import { TransactionModule } from './transaction/transaction.module.js';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
-  imports: [PrismaModule, UserModule, WalletModule, TransactionModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule,
+    UserModule,
+    WalletModule,
+    TransactionModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
